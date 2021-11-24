c663d1d99b6947630fc7152dfe79cd27df7e6d8f
Ben Davis <ben@bencdavis.com>
2021-11-23 22:58:58 -0500

    Working prototyppe demonstrating relationships and inclusions

---
+++ generics.py
@@ -0,0 +1,292 @@
+import dataclasses
+import inspect
+import typing
+from typing import (
+    Any,
+    Callable,
+    Generic,
+    List,
+    Literal,
+    Optional,
+    Protocol,
+    Type,
+    TypeVar,
+    Union,
+)
+
+from fastapi import APIRouter, HTTPException
+from pydantic.generics import GenericModel
+from sqlalchemy.future.engine import Engine
+from sqlalchemy.orm import joinedload
+from sqlmodel import Session, SQLModel, select
+
+TCreate = TypeVar("TCreate", bound=SQLModel)
+TUpdate = TypeVar("TUpdate", bound=SQLModel)
+TRead = TypeVar("TRead", bound=SQLModel)
+TIncludeParam = TypeVar("TIncludeParam", bound=str)
+TypeVarType = Any
+
+
+@dataclasses.dataclass
+class Relationship:
+    schema: SQLModel
+    many: bool
+
+
+class JAResource(GenericModel, Generic[TRead]):
+    id: Union[str, int]
+    type: str
+    attributes: TRead
+
+
+TJAResource = TypeVar("TJAResource")
+
+
+class JAResponse(GenericModel, Generic[TJAResource]):
+    data: TJAResource
+    # Todo
+    included: List[dict]
+
+
+class DBSQLModel(SQLModel):
+    id: str | int
+
+
+class SQLResource(Protocol):
+    name: str
+    engine: Engine
+
+    relationships: dict[str, Relationship] = {}
+
+    Db: Type[DBSQLModel]
+    Read: Type[SQLModel]
+
+    Create: Optional[Type[SQLModel]]
+    Update: Optional[Type[SQLModel]]
+
+    create: Optional[Callable]
+    list: Optional[Callable]
+    update: Optional[Callable]
+    delete: Optional[Callable]
+    retrieve: Optional[Callable]
+
+
+class BaseSQLResource:
+    def __init__(self: SQLResource, *args, **kwargs):
+        print("Init", self)
+        # # Build the relationships for the Db model
+        annotations = typing.get_type_hints(self.Db)
+        relationship_fields = self.Db.__sqlmodel_relationships__.keys()
+        self.relationships = {
+            field: typing.get_args(annotations[field])[0]
+            for field in relationship_fields
+        }
+
+        for field in relationship_fields:
+            annotated_type = annotations[field]
+            many = typing.get_origin(annotated_type) == list
+
+            self.relationships[field] = Relationship(
+                schema=typing.get_args(annotated_type)[0],
+                many=many,
+            )
+
+        # Params to replace
+        methods = {
+            "create": {TCreate: self.Create},
+            "update": {TUpdate: self.Update},
+            "retrieve": {TIncludeParam: Literal[tuple(relationship_fields)]},
+            "list": {TIncludeParam: Literal[tuple(relationship_fields)]},
+        }
+
+        for method_name, replacements in methods.items():
+            method_instance = getattr(self, method_name, None)
+
+            if not method_instance:
+                continue
+
+            original_signature = inspect.signature(method_instance)
+            updated_params = [
+                inspect.Parameter(
+                    name=param.name,
+                    kind=param.kind,
+                    default=param.default,
+                    annotation=replacements.get(param.annotation, param.annotation),
+                )
+                for param in original_signature.parameters.values()
+            ]
+            updated_signature = inspect.Signature(updated_params)
+
+            # Required to avoid closing over method_name
+            def factory(_method_name):
+                def wrapper(*args, **kwargs):
+                    class_method = getattr(self.__class__, _method_name)
+                    print("Class method", _method_name)
+                    return class_method(self, *args, **kwargs)
+
+                return wrapper
+
+            setattr(self, method_name, factory(method_name))
+            getattr(self, method_name).__signature__ = updated_signature
+
+        return super().__init__(*args, **kwargs)
+
+
+class CreateResourceMixin(Generic[TCreate]):
+    def create(self: SQLResource, model: TCreate):
+        with Session(self.engine) as session:
+            row = self.Db.from_orm(model)
+            session.add(row)
+            session.commit()
+            session.refresh(row)
+
+            return JAResponse(data=JAResource(id=row.id, attributes=row, type=self.name), included=[])
+
+
+class UpdateResourceMixin(Generic[TUpdate]):
+    def update(
+        self: SQLResource,
+        *,
+        id: int,
+        model: TUpdate,
+    ):
+        with Session(self.engine) as session:
+            row = session.get(self.Db, id)
+
+            if not row:
+                raise HTTPException(status_code=404, detail=f"{self.name} not found")
+
+            data = model.dict(exclude_unset=True)
+            for key, value in data.items():
+                setattr(row, key, value)
+
+            session.add(row)
+            session.commit()
+            session.refresh(row)
+
+            row = session.exec(select(self.Db).where(self.Db.id == id)).one()
+
+            return row
+
+
+class ListResourceMixin(Generic[TIncludeParam]):
+    def list(self: SQLResource, include: TIncludeParam = None):
+        with Session(self.engine) as session:
+            rows = session.exec(select(self.Db)).all()
+            return rows
+
+
+class RetrieveResourceMixin(Generic[TIncludeParam]):
+    def retrieve(self: SQLResource, *, id: int, include: TIncludeParam = None):
+        with Session(self.engine) as session:
+            inclusions = include.split(',') if include else []
+            options = []
+
+            # Build the query options based on the include
+            for inclusion in inclusions:
+                options.append(joinedload(
+                    getattr(self.Db, inclusion)
+                ))
+
+            row = session.exec(select(self.Db).where(self.Db.id == id).options(*options)).unique().one()
+            if not row:
+                raise HTTPException(
+                    status_code=404, detail=f"{self.name.title()} not found"
+                )
+
+            included = []
+            for inclusion in inclusions:
+                included_objs = getattr(row, inclusion)
+                included_objs = [included_objs] if not isinstance(included_objs, list) else included_objs
+
+                for included_obj in included_objs:
+                    schema = self.relationships[inclusion].schema
+                    included.append(
+                        JAResource(
+                            id=included_obj.id,
+                            type=inclusion,
+                            attributes=schema.from_orm(included_obj)
+                        )
+                    )
+
+            return JAResponse(data=JAResource(id=id, attributes=row, type=self.name), included=included)
+
+
+class DeleteResourceMixin:
+    def delete(self: SQLResource, *, id: int):
+        with Session(self.engine) as session:
+            row = session.get(self.Db, id)
+            if not row:
+                raise HTTPException(status_code=404, detail=f"{self.name} not found")
+
+            session.delete(row)
+            session.commit()
+
+            return {"ok": True}
+
+
+class ListCreateResource(CreateResourceMixin, ListResourceMixin, BaseSQLResource):
+    pass
+
+
+class ListCreateUpdateResource(
+    ListResourceMixin,
+    CreateResourceMixin,
+    UpdateResourceMixin,
+    BaseSQLResource,
+    Generic[TCreate, TUpdate],
+):
+    pass
+
+
+class FullResource(
+    RetrieveResourceMixin,
+    ListResourceMixin,
+    CreateResourceMixin,
+    UpdateResourceMixin,
+    DeleteResourceMixin,
+    BaseSQLResource,
+    Generic[TCreate, TUpdate],
+):
+    pass
+
+
+class ResourceRouter(APIRouter):
+    def __init__(
+        self,
+        *,
+        resource: SQLResource,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+
+        Read = resource.Read
+
+        if resource.retrieve:
+            self.get(
+                f"/{{id}}",
+                response_model=JAResponse[JAResource[Read]],
+                summary=f"Get {resource.name}",
+            )(resource.retrieve)
+
+        if resource.list:
+            Read = resource.Read
+            self.get(
+                f"/", response_model=List[Read], summary=f"Get {resource.name} list"
+            )(resource.list)
+
+        if resource.create:
+            print("CONNECTING", resource.create)
+            self.post(
+                f"/", response_model=JAResponse[JAResource[Read]], summary=f"Create {resource.name}"
+            )(resource.create)
+
+        if resource.update:
+            self.patch(
+                f"/{{id}}",
+                response_model=resource.Read,
+                summary=f"Update {resource.name}",
+            )(resource.update)
+
+        if resource.delete:
+            self.delete(f"/{{id}}", summary=f"Delete {resource.name}")(resource.delete)
--- test.py
+++ test.py
@@ -1,24 +1,9 @@
-from fastapi import FastAPI, HTTPException, Depends, APIRouter
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Callable,
-    ClassVar,
-    Dict,
-    Generic,
-    List,
-    Optional,
-    Protocol,
-    Tuple,
-    Type,
-    TypeVar,
-    Union,
-    get_args,
-    get_origin,
-    get_type_hints,
-)
-from sqlmodel import SQLModel, Field, create_engine, Session, select
-import dataclasses
+from typing import List, Optional
+
+from fastapi import FastAPI
+from sqlmodel import Field, Relationship, SQLModel, create_engine
+
+import generics
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -28,9 +13,29 @@ engine = create_engine(sqlite_url, echo=True)
 app = FastAPI()
 
 
-def get_session():
-    with Session(engine) as session:
-        yield session
+class TeamBase(SQLModel):
+    name: str
+    headquarters: str
+
+
+class Team(TeamBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    heroes: List["Hero"] = Relationship(back_populates="team")
+
+
+class TeamCreate(TeamBase):
+    pass
+
+
+class TeamRead(TeamBase):
+    id: int
+
+
+class TeamUpdate(SQLModel):
+    id: Optional[int] = None
+    name: Optional[str] = None
+    headquarters: Optional[str] = None
 
 
 class HeroBase(SQLModel):
@@ -38,9 +43,13 @@ class HeroBase(SQLModel):
     secret_name: str
     age: Optional[int] = None
 
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+
 
 class Hero(HeroBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+
+    team: Optional[Team] = Relationship(back_populates="heroes")
 
 
 class HeroCreate(HeroBase):
@@ -55,6 +64,7 @@ class HeroUpdate(SQLModel):
     name: Optional[str] = None
     secret_name: Optional[str] = None
     age: Optional[int] = None
+    team_id: Optional[int] = None
 
 
 @app.on_event("startup")
@@ -62,475 +72,35 @@ def on_startup():
     SQLModel.metadata.create_all(engine)
 
 
-# @dataclasses.dataclass
-# class Resource:
-#     name: str
-#     Db: Type
-
-#     Read: Type = None
-#     Create: Optional[Type] = None
-#     Update: Optional[Type] = None
-
-#     retrieve: Optional[Callable] = None
-#     list: Optional[Callable] = None
-#     create: Optional[Callable] = None
-#     update: Optional[Callable] = None
-#     delete: Optional[Callable] = None
-
-
-# def build_resource(*funcs: Callable, **kwargs):
-#     resource = Resource(**kwargs)
-
-#     for func in funcs:
-#         resource = func(resource=resource, **kwargs)
-
-#     return resource
-
-
-# def with_sqlmodel_create(*, resource: Resource, Db: Type, Create: Type, **kwargs):
-#     def create(*, session: Session = Depends(get_session), model: Create):
-#         row = Db.from_orm(model)
-#         session.add(row)
-#         session.commit()
-#         session.refresh(row)
-#         return row
-
-#     resource.create = create
-
-#     return resource
-
-
-# def with_sqlmodel_retrieve(*, resource: Resource, Db: Type, name: str, **kwargs):
-#     def retrieve(*, session: Session = Depends(get_session), id: int):
-#         row = session.get(Db, id)
-#         if not row:
-#             raise HTTPException(status_code=404, detail=f"{name.title()} not found")
-
-#         return row
-
-#     resource.retrieve = retrieve
-
-#     return resource
-
-
-# def with_sqlmodel_list(*, resource: Resource, Db: Type, **kwargs):
-#     def list(session: Session = Depends(get_session)):
-#         rows = session.exec(select(Db)).all()
-#         return rows
-
-#     resource.list = list
-
-#     return resource
-
-
-# def with_sqlmodel_update(
-#     *, resource: Resource, Db: Type, Update: Type, name: str, **kwargs
-# ):
-#     def update(*, session: Session = Depends(get_session), id: int, model: Update):
-#         row = session.get(Db, id)
-
-#         if not row:
-#             raise HTTPException(status_code=404, detail=f"{name} not found")
-
-#         data = model.dict(exclude_unset=True)
-#         for key, value in data.items():
-#             setattr(row, key, value)
-
-#         session.add(row)
-#         session.commit()
-#         session.refresh(row)
-
-#         return row
-
-#     resource.update = update
-
-#     return resource
-
-
-# def with_sqlmodel_destroy(*, resource: Resource, Db: Type, name: str, **kwargs):
-#     def delete(*, session: Session = Depends(get_session), id: int):
-#         row = session.get(Db, id)
-#         if not row:
-#             raise HTTPException(status_code=404, detail=f"{name} not found")
-
-#         session.delete(row)
-#         session.commit()
-
-#         return {"ok": True}
-
-#     resource.delete = delete
-
-#     return resource
-
-
-# def sqlmodel_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-#     Create: Type,
-#     Update: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_retrieve,
-#         with_sqlmodel_list,
-#         with_sqlmodel_create,
-#         with_sqlmodel_update,
-#         with_sqlmodel_destroy,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#         Create=Create,
-#         Update=Update,
-#     )
-
-
-# def sqlmodel_create_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-#     Create: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_create,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#         Create=Create,
-#     )
-
-
-# def sqlmodel_list_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_list,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#     )
-
-
-# def sqlmodel_retrieve_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_retrieve,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#     )
-
-
-# def sqlmodel_read_only_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_retrieve,
-#         with_sqlmodel_list,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#     )
-
-
-# def sqlmodel_update_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-#     Update: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_update,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#         Update=Update,
-#     )
-
-
-# def sqlmodel_destroy_resource(
-#     name: str,
-#     Db: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_destroy,
-#         name=name,
-#         Db=Db,
-#     )
-
-
-# def sqlmodel_list_create_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-#     Create: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_list,
-#         with_sqlmodel_create,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#         Create=Create,
-#     )
-
-
-# def sqlmodel_retrieve_update_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-#     Update: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_retrieve,
-#         with_sqlmodel_update,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#         Update=Update,
-#     )
-
-
-# def sqlmodel_retrieve_destroy_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_retrieve,
-#         with_sqlmodel_destroy,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#     )
-
-
-# def sqlmodel_retrieve_update_destroy_resource(
-#     name: str,
-#     Db: Type,
-#     Read: Type,
-#     Update: Type,
-# ):
-#     return build_resource(
-#         with_sqlmodel_retrieve,
-#         with_sqlmodel_update,
-#         with_sqlmodel_destroy,
-#         name=name,
-#         Db=Db,
-#         Read=Read,
-#         Update=Update,
-#     )
-
-
-TCreate = TypeVar("TCreate", bound=SQLModel)
-TUpdate = TypeVar("TUpdate", bound=SQLModel)
-TypeVarType = Any
-
-
-class SQLResource(Protocol):
-    name: str
-
-    Db: Type[SQLModel]
-    Read: Type[SQLModel]
-    # Create: Type[SQLModel]
-
-    create: Optional[Callable]
-    list: Optional[Callable]
-    update: Optional[Callable]
-    delete: Optional[Callable]
-    retrieve: Optional[Callable]
-
-
-class BaseSQLResource:
-    create: Optional[Callable] = None
-    list: Optional[Callable] = None
-    update: Optional[Callable] = None
-    delete: Optional[Callable] = None
-    retrieve: Optional[Callable] = None
-
-    if TYPE_CHECKING:
-        # Putting this in a TYPE_CHECKING block allows us to replace `if Generic not in cls.__bases__` with
-        # `not hasattr(cls, "__parameters__")`. This means we don't need to force non-concrete subclasses of
-        # `GenericModel` to also inherit from `Generic`, which would require changes to the use of `create_model` below.
-        __parameters__: ClassVar[Tuple[TypeVarType, ...]]
-
-    # def __new__(cls, *args, **kwargs):
-    #     breakpoint()
-    #     super().__new__(cls, *args, **kwargs)
-    def __class_getitem__(cls, params):
-        methods = ["create", "list", "update", "delete", "retrieve"]
-
-        if not isinstance(params, tuple):
-            params = (params,)
-
-        typevars_map: Dict[TypeVarType, Type[Any]] = dict(
-            zip(cls.__parameters__, params)
-        )
-
-        for method_name in methods:
-            method = getattr(cls, method_name, None)
-
-            if not method:
-                continue
-
-            for attr, t in method.__annotations__.items():
-                if t in typevars_map:
-                    print("repalcing", attr, typevars_map[t])
-                    method.__annotations__[attr] = typevars_map[t]
-
-        return cls
-
-    # def __init__(self, name: str, Db: Type, Read: Type):
-    #     self.name = name
-    #     self.Db = Db
-    #     self.Read = Read
-
-
-class CreateResourceMixin(BaseSQLResource, Generic[TCreate]):
-    def create(self: SQLResource, *, session: Session = Depends(get_session), model: TCreate):
-        row = self.Db.from_orm(model)
-        session.add(row)
-        session.commit()
-        session.refresh(row)
-        return row
-
-
-class UpdateResourceMixin(BaseSQLResource, Generic[TUpdate]):
-    def update(
-        self: SQLResource,
-        *,
-        session: Session = Depends(get_session),
-        id: int,
-        model: TUpdate,
-    ):
-        row = session.get(self.Db, id)
-
-        if not row:
-            raise HTTPException(status_code=404, detail=f"{self.name} not found")
-
-        data = model.dict(exclude_unset=True)
-        for key, value in data.items():
-            setattr(row, key, value)
-
-        session.add(row)
-        session.commit()
-        session.refresh(row)
-
-        return row
-
-
-class ListResourceMixin:
-    def list(self: SQLResource, session: Session = Depends(get_session)):
-        rows = session.exec(select(self.Db)).all()
-        return rows
-
-
-class RetrieveResourceMixin:
-    def retrieve(self: SQLResource, *, session: Session = Depends(get_session), id: int):
-        row = session.get(self.Db, id)
-        if not row:
-            raise HTTPException(status_code=404, detail=f"{self.name.title()} not found")
-
-        return row
-
-
-class DeleteResourceMixin:
-    def delete(self: SQLResource, *, session: Session = Depends(get_session), id: int):
-        row = session.get(self.Db, id)
-        if not row:
-            raise HTTPException(status_code=404, detail=f"{self.name} not found")
-
-        session.delete(row)
-        session.commit()
-
-        return {"ok": True}
-
-
-
-class ListCreateResource(CreateResourceMixin[TCreate], ListResourceMixin, BaseSQLResource):
-    pass
-
-
-class ListCreateUpdateResource(
-    ListResourceMixin,
-    CreateResourceMixin[TCreate],
-    UpdateResourceMixin[TUpdate],
-    BaseSQLResource,
-    Generic[TCreate, TUpdate],
-):
-    pass
-
-
-class FullResource(
-    RetrieveResourceMixin,
-    ListResourceMixin,
-    CreateResourceMixin[TCreate],
-    UpdateResourceMixin[TUpdate],
-    DeleteResourceMixin,
-    BaseSQLResource,
-    Generic[TCreate, TUpdate],
-):
-    pass
-
-
-class ResourceRouter(APIRouter):
-    def __init__(
-        self,
-        *,
-        resource: SQLResource,
-        **kwargs,
-    ) -> None:
-        super().__init__(**kwargs)
-
-        if resource.retrieve:
-            self.get(
-                f"/{{id}}", response_model=resource.Read, summary=f"Get {resource.name}"
-            )(resource.retrieve)
-
-        if resource.list:
-            Read = resource.Read
-            self.get(
-                f"/", response_model=List[Read], summary=f"Get {resource.name} list"
-            )(resource.list)
-
-        if resource.create:
-            self.post(
-                f"/", response_model=resource.Read, summary=f"Create {resource.name}"
-            )(resource.create)
-
-        if resource.update:
-            self.patch(
-                f"/{{id}}",
-                response_model=resource.Read,
-                summary=f"Update {resource.name}",
-            )(resource.update)
-
-        if resource.delete:
-            self.delete(f"/{{id}}", summary=f"Delete {resource.name}")(resource.delete)
-
-
-class HeroResource(FullResource[HeroCreate, HeroUpdate]):
+class TeamResource(generics.FullResource):
+    name = "team"
+    engine = engine
+    Db = Team
+    Read = TeamRead
+    Create = TeamCreate
+    Update = TeamUpdate
+
+class HeroResource(generics.FullResource):
     name = "hero"
+    engine = engine
     Db = Hero
     Read = HeroRead
+    Create = HeroCreate
+    Update = HeroUpdate
 
-    def list(self: SQLResource, session: Session = Depends(get_session)):
-        print("LISTING")
-        return super().list(session=session)
 
-resource = HeroResource()
 
-hero = ResourceRouter(prefix="/heroes", resource=resource)
-# yolo = ResourceRouter(name="yolo", plural_name="yoloes", Db=Hero, Read=HeroRead)
+
+hero_resource = HeroResource()
+team_resource = TeamResource()
+
+team = generics.ResourceRouter(prefix="/teams", resource=team_resource, tags=["Teams"])
+hero = generics.ResourceRouter(
+    prefix="/heroes", resource=hero_resource, tags=["Heroes"]
+)
 
 app.include_router(hero)
-# app.include_router(yolo)
+app.include_router(team)
 
 
 """
